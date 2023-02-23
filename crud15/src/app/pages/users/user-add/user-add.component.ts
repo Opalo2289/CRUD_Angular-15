27f18d22a6524a837ae720b79cb249ee57b3b6f4
Opalo1189 <enriquemaestre11@gmail.com>
2023-02-22 19:47:22 -0500

    Sesion 4-Desarrollo-User-add

--- crud15/src/app/pages/users/user-add/user-add.component.ts
+++ crud15/src/app/pages/users/user-add/user-add.component.ts
@@ -1,13 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { FormArray, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { PlayersService } from 'src/app/services/players.service';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { Player } from 'src/app/common/interface/player.interface';
 @Component({
   selector: 'app-user-add',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './user-add.component.html',
   styleUrls: ['./user-add.component.css']
 })
 export class UserAddComponent {
+  _playerService = inject(PlayersService)
+  _rooter = inject(Router)
 
+  form = new FormGroup (
+    {
+      name: new FormControl('', Validators.required),
+      decks: new FormArray([]),
+    }
+  );
+
+  get decks() {
+    return(this.form.get('decks') as FormArray).controls //Castear como un formArray (esta devolviendo los controles del formulario "decks") el template de angular suele tener problemas identificando este tipo de funcionalidades dentro de un ngfor
+    //En un inicio el 'decks' estará vacio
+  }
+
+  createDeck() {
+    (this.form.get('decks') as FormArray).push(
+      new FormGroup({
+        name: new FormControl('', Validators.required),
+        cards: new FormControl(null, Validators.required) //Null porque va estar ligado a numeros no ha cadenas 
+      })
+    )
+  }
 }
